725a4c4950db0f7fb4921fd2fcc8e539dbfa9594
Harshal-creator <harshalshelar8251@gmail.com>
2024-02-04 12:19:13 +0530

    added scss file in nav js

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import './Nav.css';
+import './Nav.scss';
 import profileImg from '../../Assets/Images/pro3Trans.png';
 
 
@@ -14,14 +14,6 @@ const Nav = () => {
   useEffect(() => {
     getNotificationCount();
   },[notCount])
-
-  const openSidebar = () => {
-    setSidebar(true);
-  }
-
-  const closeSidebar = () => {
-    setSidebar(false);
-  }
 
   const openNotification = () => {
     setNotPopup(true);
@@ -49,35 +41,35 @@ const Nav = () => {
           <div>
             <ul className='nav-ul'>
               <div className="all-li">
-                <li className='nav-li logoOnly'><Link to="/">MERN</Link></li>
-                <li className='nav-li'><Link to="/">Product List</Link></li>
-                <li className='nav-li'><Link to="/addProducts">Add Products</Link></li>
-                <li className='nav-li'><Link to="/categories">Categories</Link></li>
+                <li className='logoOnly'><Link to="/">MERN</Link></li>
+                <li><Link to="/">Product List</Link></li>
+                <li><Link to="/addProducts">Add Products</Link></li>
+                <li><Link to="/categories">Categories</Link></li>
               </div>
               <div className='loginLogout'>
-                <li className='nav-li'><i className="fa fa-bell-o" onClick={openNotification}></i><span className='notificationCount'>{notCount}</span></li>
-                <li className='nav-li'><Link to="/profile"> <i className="fa fa-user-circle-o"></i> {JSON.parse(auth).name}</Link></li>
+                <li><i className="fa fa-bell-o" onClick={openNotification}></i><span className='notificationCount'>{notCount}</span></li>
+                <li><Link to="/profile"> <i className="fa fa-user-circle-o"></i> {JSON.parse(auth).name}</Link></li>
               </div>
             </ul>
             <div className="respHeader">
               <img className='respProfileImg' src={profileImg} alt='' />
               <span className="respHeading">MERN CRUD</span>
               {!sidebar ?
-                <i className='fa fa-chevron-left' onClick={openSidebar}></i> : <></>
+                <i className='fa fa-chevron-left' onClick={()=>setSidebar(true)}></i> : <></>
               }
 
               {sidebar ?
-                <i className='fa fa-chevron-right' onClick={closeSidebar}></i> : <></>
+                <i className='fa fa-chevron-right' onClick={()=>setSidebar(false)}></i> : <></>
               }
             </div>
             {
               sidebar ?
                 <div className="respHeaderData">
                   <ul className='respUl'>
-                    <li className='respLi' onClick={closeSidebar}><Link to="/"> <i className='fa fa-server'></i> Product List</Link></li>
-                    <li className='respLi' onClick={closeSidebar}><Link to="/addProducts"> <i className='fa fa-user-plus'></i> Add Product</Link></li>
-                    <li className='respLi' onClick={()=>{closeSidebar();openNotification()}}><Link to="/addProducts"> <i className='fa fa-bell-o'></i> {notCount} Notifications</Link></li>
-                    <li className='respLi' onClick={closeSidebar}><Link to="/profile"> <i className='fa fa-user-circle-o'></i> {JSON.parse(auth).name}</Link></li>
+                    <li onClick={()=>setSidebar(false)}><Link to="/"> <i className='fa fa-server'></i> Product List</Link></li>
+                    <li onClick={()=>setSidebar(false)}><Link to="/addProducts"> <i className='fa fa-user-plus'></i> Add Product</Link></li>
+                    <li onClick={()=>{setSidebar(false);openNotification()}}><Link to="/addProducts"> <i className='fa fa-bell-o'></i> {notCount} Notifications</Link></li>
+                    <li onClick={()=>setSidebar(false)}><Link to="/profile"> <i className='fa fa-user-circle-o'></i> {JSON.parse(auth).name}</Link></li>
                   </ul>
                 </div> :
                 <></>
